4ac857317e54da2b94613ae9ff606d24509bdc9e
Rahul Yadav <136972250+Rahul-Yadav-88@users.noreply.github.com>
2024-11-11 06:02:04 +0000

    proj

--- 7_Projects/01.js
+++ 7_Projects/01.js
@@ -1,6 +1,31 @@
+// Write a program to greeat a person based on their grnder (if else)
+// let a = "man"
+// let b = "women" 
+// let c = "women"
+// if (c === a) {
+//     console.log ("Hlo mr")
+// } else if (c===b){
+//     console.log("Hlo ms")
+// }else{
+//     console.log("Hlo")
+// }
+
+// function greetPerson(name, gender) {
+//     if (gender.toLowerCase() === 'male') {
+//         console.log(`Hello Mr. ${name}`);
+//     } else if (gender.toLowerCase() === 'female') {
+//         console.log(`Hello Ms. ${name}`);
+//     } else {
+//         console.log(`Hello ${name}`);
+//     }
+// }
+// greetPerson("Sonu","male");
+// greetPerson("Gaurav","female");
+// greetPerson('abc',"other")
+
 // Write a Program that counts from 1 to 1000 and prints (for loop)
-let answer =0
-for (let i=0; i<=100; i++){
-    answer = answer +i
-}
-console.log(answer)
+// let answer =0
+// for (let i=0; i<=100; i++){
+//     answer = answer +i
+// }
+// console.log(answer)
